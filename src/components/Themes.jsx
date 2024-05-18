a23e34ff01849921156318c33fa91e828c0d4a0a
Rahul <chattychidiya@gmail.com>
2024-05-18 18:47:33 +0530

    Refactor Themes component to improve accessibility and styling

--- src/components/Themes.jsx
+++ src/components/Themes.jsx
@@ -11,27 +11,30 @@ function Themes() {
 			<div
 				tabIndex={0}
 				role="button"
-				className="btn btn-accent text-accent-content w-full  px-5"
+				className="btn btn-accent text-accent-content w-full px-5"
 			>
 				Themes
 			</div>
-			<ul
+			<div
 				tabIndex={0}
-				className="dropdown-content menu bg-base-100 rounded-box [&>li]:text-base-content z-[1] mt-2 w-40 p-2 shadow-md"
+				className="no-scrollbar dropdown-content menu bg-base-100 rounded-box text-base-content z-[1] mt-2 h-80 w-40 overflow-y-auto p-2 shadow-md"
 			>
-				{themes.map((theme, key) => (
-					<li key={key} className="mb-2">
-						<a
-							onClick={(e) => {
-								changeTheme(theme);
-								localStorage.setItem("theme", theme);
-							}}
-						>
-							{theme[0].toUpperCase() + theme.slice(1)}
-						</a>
-					</li>
-				))}
-			</ul>
+				<ul className="m-0 list-none p-0">
+					{themes.map((theme, key) => (
+						<li key={key} className="mb-2">
+							<a
+								className="block"
+								onClick={() => {
+									changeTheme(theme);
+									localStorage.setItem("theme", theme);
+								}}
+							>
+								{theme[0].toUpperCase() + theme.slice(1)}
+							</a>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 }
